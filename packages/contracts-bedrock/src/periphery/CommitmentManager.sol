e7fbcf01ceee8147fafcb80396c67eb3977beb99
Diego <0xfuturistic@pm.me>
2023-09-06 13:56:40 +0200

    contracts-bedrock: delete CommitmentManager

--- packages/contracts-bedrock/src/periphery/CommitmentManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-import {CommitmentManager as Manager} from "emily/CommitmentManager.sol";
-
-contract CommitmentManager is Manager {
-    constructor(uint256 accountCommitmentsGasLimit) Manager(accountCommitmentsGasLimit) {}
-}
