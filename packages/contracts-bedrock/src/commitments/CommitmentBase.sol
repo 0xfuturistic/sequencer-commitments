a70637912428af9a939e66c5ef050ada43a3c143
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-09-11 09:07:50 +0200

    contracts-bedrock: create sample FeeRecipientCommitment

---
+++ packages/contracts-bedrock/src/commitments/CommitmentBase.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import { L2OutputOracle } from "../L1/L2OutputOracle.sol";
+
+contract CommitmentBase {
+    struct ExecutionPayload {
+        bytes32 ParentHash;
+        address FeeRecipient;
+        bytes32 StateRoot;
+        bytes32 ReceiptsRoot;
+        bytes LogsBloom;
+        bytes32 PrevRandao;
+        uint64 BlockNumber;
+        uint64 GasLimit;
+        uint64 GasUsed;
+        uint64 Timestamp;
+        bytes ExtraData;
+        bytes32 BaseFeePerGas;
+        bytes32 BlockHash;
+        bytes Transactions;
+    }
+
+    L2OutputOracle public l2OutputOracle;
+
+    constructor(address l2OutputOracle_) {
+        l2OutputOracle = L2OutputOracle(l2OutputOracle_);
+    }
+}
---
+++ packages/contracts-bedrock/src/commitments/samples/FeeRecipientCommitment.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {CommitmentBase} from "../CommitmentBase.sol";
+
+contract FeeRecipientCommitment is CommitmentBase {
+    mapping(address => mapping(uint256 => bool)) public feeRecipientIsSet;
+    mapping(address => mapping(uint256 => address)) public feeRecipientSet;
+
+    event NewFeeRecipientSet(address sequencer, address feeRecipient, uint64 blockNumber);
+
+    constructor(address l2OutputOracle_) CommitmentBase(l2OutputOracle_) {}
+
+    function setNewFeeRecipient(uint64 blockNumber, address feeRecipient) external {
+        require(!feeRecipientIsSet[msg.sender][blockNumber], "Fee recipient already set");
+        feeRecipientSet[msg.sender][blockNumber] = feeRecipient;
+        emit NewFeeRecipientSet(msg.sender, feeRecipient, blockNumber);
+    }
+
+    function commitmentIndicatorFun(bytes memory rawPayload) external view returns (uint256) {
+        // get sequencer from l2OutputOracle, just like in op-node
+        address sequencer = l2OutputOracle.PROPOSER();
+        // decode payload
+        (ExecutionPayload memory executionPayload) = abi.decode(rawPayload, (ExecutionPayload));
+        // get fields from payload
+        uint64 blockNumber = executionPayload.BlockNumber;
+        address payloadFeeRecipient = executionPayload.FeeRecipient;
+        // check if fee recipient is the same as the one committed by the sequencer, if any
+        if (
+            !feeRecipientIsSet[sequencer][blockNumber] || payloadFeeRecipient == feeRecipientSet[sequencer][blockNumber]
+        ) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
